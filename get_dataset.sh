3468f5fc16e506d17579cb35bd78d83b6680cf86
Kerui Zhu <keruiz2@owl2.cs.illinois.edu>
2021-11-25 16:33:20 -0600

    add get_dataset.sh

---
+++ get_dataset.sh
@@ -0,0 +1,6 @@
+wget https://gitlab.com/shimorina/webnlg-dataset/-/archive/master/webnlg-dataset-master.zip?path=release_v2.1/json -O webnlg_dataset.zip
+unzip webnlg_dataset.zip -d webnlg_dataset
+mv webnlg_dataset/*/*/*/* webnlg_dataset
+rm webnlg_dataset.zip
+cd webnlg_dataset
+rm -rf */
