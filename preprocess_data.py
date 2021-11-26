5401da6cc63be559f085fb76642118e45a4d894d
Kerui Zhu <keruiz2@owl2.cs.illinois.edu>
2021-11-26 02:37:16 -0600

    add preprocess_data

---
+++ preprocess_data.py
@@ -0,0 +1,61 @@
+import json
+import tqdm
+
+
+raw_train_data_file = 'webnlg_dataset/webnlg_release_v2.1_train.json'
+raw_dev_data_file = 'webnlg_dataset/webnlg_release_v2.1_dev.json'
+raw_test_data_file = 'webnlg_dataset/webnlg_release_v2.1_test.json'
+
+processed_train_data_file = 'webnlg_dataset/webnlg_train.jsonl'
+processed_dev_data_file = 'webnlg_dataset/webnlg_dev.jsonl'
+processed_test_data_file = 'webnlg_dataset/webnlg_test.jsonl'
+
+
+def generate_seq(data:list):
+    parent = {}
+    children = {}
+    relations = {}
+    for tri in data:
+        sub, obj, rel = tri['subject'], tri['object'], tri['property']
+        if sub == obj:
+            return [sub, rel, obj]
+        parent[obj] = sub
+        if sub not in children:
+            children[sub] = []
+        children[sub].append(obj)
+        relations[(sub, obj)] = rel
+    root = list(parent.keys())[0]
+    while root in parent:
+        root = parent[root]
+    seq = []
+    def dfs(root:str):
+        seq.append(root)
+        if root in children:
+            for child in children[root]:
+                seq.append(relations[(root, child)])
+                dfs(child)
+    dfs(root)
+    return seq
+
+
+if __name__ == '__main__':
+    file_dict = {'Train' : (raw_train_data_file, processed_train_data_file), 
+                 'Dev' : (raw_dev_data_file, processed_dev_data_file), 
+                 'Test' : (raw_test_data_file, processed_test_data_file)}
+    for k, v in file_dict.items():
+        print('Processing %s file' % k)
+        raw_data = json.load(open(v[0]))['entries']
+        data_samples = [data_sample[list(data_sample.keys())[0]] for data_sample in raw_data]
+        
+        new_data_samples = []
+        for data_sample in tqdm.tqdm(data_samples):
+            new_data_sample = data_sample.copy()
+            unused_labels = ['originaltriplesets', 'xml_id', 'size', 'shape', 'shape_type']
+            for label in unused_labels:
+                new_data_sample.pop(label)
+            triples = new_data_sample.pop('modifiedtripleset')
+            new_data_sample['input_seq'] = generate_seq(triples)
+            new_data_sample['target_sents'] = [sent['lex'] for sent in new_data_sample.pop('lexicalisations')]
+            new_data_samples.append(new_data_sample)
+        with open(v[1], 'w') as f_out:
+            f_out.write('\n'.join([json.dumps(data) for data in new_data_samples]))
